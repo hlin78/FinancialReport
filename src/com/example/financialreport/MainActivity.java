ea4e06db545818417f0b8917e6f811ea38bebdc3
hailin <hlin78@gatech.edu>
2014-02-01 23:13:24 -0500

    mainpage
    updated main activity page

--- src/com/example/financialreport/MainActivity.java
+++ src/com/example/financialreport/MainActivity.java
@@ -3,13 +3,36 @@ package com.example.financialreport;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button register, logIn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		register = (Button) findViewById(R.id.bRegister);
+		logIn = (Button) findViewById(R.id.bLogin);
+		
+		register.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		logIn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Override
