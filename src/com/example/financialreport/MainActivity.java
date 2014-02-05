e6b904ff8e3317e4b4b0667fc8d8212a5cef931f
Farong Cheng <cfr009@163.com>
2014-02-04 23:26:42 -0500

    loininpage

--- src/com/example/financialreport/MainActivity.java
+++ src/com/example/financialreport/MainActivity.java
@@ -2,25 +2,28 @@ package com.example.financialreport;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	Button register, logIn;
+	TextView aaa;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		register = (Button) findViewById(R.id.bRegister);
 		logIn = (Button) findViewById(R.id.bLogin);
-		
+		aaa = (TextView) findViewById(R.id.textView1);
 		register.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				
 				
 			}
 		});
@@ -29,8 +32,8 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				Intent logpage = new Intent("com.example.financialreport.LOGIN");
+				startActivity(logpage);
 			}
 		});
 	}
