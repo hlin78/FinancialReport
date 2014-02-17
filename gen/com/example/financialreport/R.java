2db1de1e5b4958d0f5f9534762ad9fb5a1369f4d
whu48 <vinci.huu@gmail.com>
2014-02-16 19:30:08 -0500

    layout modification

--- gen/com/example/financialreport/R.java
+++ gen/com/example/financialreport/R.java
@@ -54,10 +54,12 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int limitation=0x7f05000a;
         public static final int login_button=0x7f050004;
         public static final int login_note=0x7f050008;
         public static final int password=0x7f050006;
         public static final int register_button=0x7f050003;
+        public static final int required=0x7f050009;
         public static final int username=0x7f050005;
         public static final int welcome_screen=0x7f050007;
     }
